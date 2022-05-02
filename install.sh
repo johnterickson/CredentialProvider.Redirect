f334f36e9c672898857c0d3bf634faf8b3536cd3
John Erickson <john@jerick-hpz440.localdomain>
2022-05-02 16:42:04 -0700

    Update to dotnet6

--- install.sh
+++ install.sh
@@ -7,13 +7,13 @@ rm -Rf $HOME/.nuget/plugins/netfx/CredentialProvider*
 echo install linux side
 dotnet build
 mkdir --parents $HOME/.nuget/plugins/netcore/CredentialProvider.Redirect
-cp -R bin/Debug/net5.0/* $HOME/.nuget/plugins/netcore/CredentialProvider.Redirect/
+cp -R bin/Debug/net6.0/* $HOME/.nuget/plugins/netcore/CredentialProvider.Redirect/
 
 echo install windows side
-dotnet build -r win10-x64
+dotnet build -r win10-x64 --self-contained
 export USERPROFILE=$(echo $(cmd.exe /c echo %USERPROFILE% 2>/dev/null) | sed 's/\\/\//g' | sed 's/\(.\):/\/mnt\/\L\1/g' | sed 's/\r//g')
 mkdir --parents "$USERPROFILE/.nuget/plugins/helpers/CredentialProvider.Redirect/"
-cp -R bin/Debug/net5.0/win10-x64/* $USERPROFILE/.nuget/plugins/helpers/CredentialProvider.Redirect/
+cp -R bin/Debug/net6.0/win10-x64/* $USERPROFILE/.nuget/plugins/helpers/CredentialProvider.Redirect/
 
 echo override Linux CredentialProvider.Redirect to go to Windows CredentialProvider.Redirect
 echo export NUGET_CREDENTIALPROVIDER_REDIRECT_TARGET=$USERPROFILE/.nuget/plugins/helpers/CredentialProvider.Redirect/CredentialProvider.Redirect.exe >> ~/.bashrc
